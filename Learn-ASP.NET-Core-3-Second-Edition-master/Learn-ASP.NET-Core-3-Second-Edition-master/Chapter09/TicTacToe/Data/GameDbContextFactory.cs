a172405a6879727f132f7919bc148aaab4601e5c
Dave Robb <dmrobb.60@live.com>
2020-02-27 13:23:34 -0500

    2020-02-27 Office
    Chapter 4 Page 132

---
+++ Learn-ASP.NET-Core-3-Second-Edition-master/Learn-ASP.NET-Core-3-Second-Edition-master/Chapter09/TicTacToe/Data/GameDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TicTacToe.Data
+{
+    public class GameDbContextFactory :  IDesignTimeDbContextFactory<GameDbContext>
+    {
+        public GameDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder =
+             new DbContextOptionsBuilder<GameDbContext>();
+            optionsBuilder.UseSqlServer(@"Server= (localdb)\MSSQLLocalDB;Database=TicTacToe; Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new GameDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Learn-ASP.NET-Core-3-Second-Edition-master/Learn-ASP.NET-Core-3-Second-Edition-master/Chapter09/TicTacToe/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicTacToe.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "FirstNameRequired")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "LastNameRequired")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "EmailRequired"),
+         DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "PasswordRequired"),
+         DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public System.DateTime? EmailConfirmationDate { get; set; }
+        public int Score { get; set; }
+    }
+}
